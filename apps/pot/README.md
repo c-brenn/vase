874be2a6d9338fe6a8f222fb0fc0ac5d793b2c80
c-brenn <brennc27@tcd.ie>
2016-11-20 18:32:14 +0000

    add application for file server

---
+++ apps/pot/README.md
@@ -0,0 +1,4 @@
+# Pot
+
+File Server and Directory Server rolled into one. Connect Pot nodes together to form a DFS.
+
---
+++ apps/pot/test/pot_test.exs
@@ -0,0 +1,8 @@
+defmodule PotTest do
+  use ExUnit.Case
+  doctest Pot
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
