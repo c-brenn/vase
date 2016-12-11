577ebd774d790744652e89f6375d52ee31f3be7c
c-brenn <brennc27@tcd.ie>
2016-12-11 07:53:46 +0000

    frontend now opens a channel connection `directories:CWD`

--- apps/urn/web/elm/Messages.elm
+++ apps/urn/web/elm/Messages.elm
@@ -1,5 +1,9 @@
 module Messages exposing (..)
 
 import Navigation exposing (Location)
+import Models     exposing (File, Directory)
 
-type Msg = OnLocationChange Location
+type Msg
+  = OnLocationChange Location
+  | DirectoryListing (List Directory) (List File)
+  | NoOp
--- apps/urn/web/static/js/app.js
+++ apps/urn/web/static/js/app.js
@@ -1,3 +1,38 @@
 import "phoenix_html"
+import {Socket} from "phoenix"
+
 const elmDiv = document.getElementById('elm-container')
 const elmApp = Elm.Main.embed(elmDiv);
+
+let socket = new Socket("/socket")
+socket.connect()
+
+let channel = {leave: () => {}}
+
+elmApp.ports.cd.subscribe(directory => {
+  channel.leave()
+  channel = socket.channel(`directories:${directory}`, {})
+  initializeChannel(channel)
+})
+
+let initializeChannel = channel => {
+  channel.on("ls", dir_contents => {
+    console.log `received contents: ${dir_contents}`
+    elmApp.ports.rawDirectoryEvents.send(
+      {
+        event: 'ls',
+        directories: dir_contents.directories,
+        files: dir_contents.files
+      }
+    )
+  })
+
+
+  channel.on("presence_diff", diff => {
+    console.log `Received diff: ${diff}`
+  })
+
+  channel.join()
+}
+
+
